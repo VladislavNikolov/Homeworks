3b4b46b57c5c18d4d112cf03d61a2a4d7f938725
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-15 14:17:27 +0300

    Added Palindromes.cs and LettersChangeNumbers.cs

---
+++ Advanced C#/4. Strings-And-Text-Processing/F. Palindromes/Palindromes.cs
@@ -0,0 +1,48 @@
+﻿using  System;
+using System.Collections.Generic;
+
+class Palindromes
+{
+    static void Main()
+    {
+        string[] allWords = Console.ReadLine().Split(new char[] 
+        {' ', ',', '.', '?', '!'}, StringSplitOptions.RemoveEmptyEntries);
+        SortedSet<string> palindromes = new SortedSet<string>();
+        string firstHalf;
+        string secondalf;
+
+        for (int i = 0; i < allWords.Length; i++)
+        {
+            if (allWords[i].Length == 1)//If the word has only one letter add it to palindromes
+            {
+                palindromes.Add(allWords[i]);
+            }
+            else
+            {
+                firstHalf = allWords[i].Substring(0, allWords[i].Length/2);
+                if (allWords[i].Length%2 == 0)//if the word has even number of characters
+                {
+                    secondalf = GetReversedString(allWords[i].Substring(allWords[i].Length/2));
+                }
+                else////if the word has odd number of characters
+                {
+                    secondalf = GetReversedString(allWords[i].Substring(allWords[i].Length / 2 + 1));
+                }
+                if (firstHalf == secondalf)//If both halves match add to palindromes
+                {
+                    palindromes.Add(allWords[i]);
+                }
+            }
+        }
+        Console.WriteLine(String.Join(", ", palindromes));
+    }
+
+    static string GetReversedString(string line)
+    {
+        char[] reversedChars = line.ToCharArray();
+        Array.Reverse(reversedChars);
+        string reversed = new string(reversedChars);
+
+        return reversed;
+    }
+}
---
+++ Advanced C#/4. Strings-And-Text-Processing/G. LettersChangeNumbers/LettersChangeNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class LettersChangeNumbers
+{
+    static void Main()
+    {
+        string[] allWords = Console.ReadLine().Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
+        decimal currentNum, currentChar;
+        decimal sum = 0;
+
+        for (int i = 0; i < allWords.Length; i++)
+        {
+            currentNum = decimal.Parse(allWords[i].Substring(1, allWords[i].Length - 2));
+            
+            if (char.IsUpper(allWords[i][0]))
+            {
+                currentChar = allWords[i][0] - '@';
+                currentNum /= currentChar;
+            }
+            else
+            {
+                currentChar = allWords[i][0] - '`';
+                currentNum *= currentChar;
+            }
+            if (char.IsUpper(allWords[i][allWords[i].Length - 1]))
+            {
+                currentChar = allWords[i][allWords[i].Length - 1] - '@';
+                currentNum -= currentChar;
+            }
+            else
+            {
+                currentChar = allWords[i][allWords[i].Length - 1] - '`';
+                currentNum += currentChar;
+            }
+            sum += currentNum;
+        }
+        Console.WriteLine("{0:F2}", sum);
+    }
+}
