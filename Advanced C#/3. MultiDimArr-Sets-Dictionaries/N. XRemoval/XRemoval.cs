568a45056aa88f0e1602e997548b3caad8947df9
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-18 16:47:18 +0300

    Added XRemoval.cs and VladkoNotebook.cs

---
+++ Advanced C#/3. MultiDimArr-Sets-Dictionaries/N. XRemoval/XRemoval.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PlusRemove
+{
+    static void Main()
+    {
+        //Fill the matrix
+        List<char[]> matrix = new List<char[]>();
+        string matrixLine = Console.ReadLine();
+        while (matrixLine != "END")
+        {
+            matrix.Add(matrixLine.ToCharArray());
+
+            matrixLine = Console.ReadLine();
+        }
+
+        //Get locations
+        HashSet<KeyValuePair<int, int>> coordinatesHS = new HashSet<KeyValuePair<int, int>>();
+        for (int row = 0; row < matrix.Count - 2; row++)
+        {
+            for (int col = 0; col < matrix[row].Length - 2; col++)
+            {
+                if (IsPlus(matrix, row, col))
+                {
+                    coordinatesHS.Add(new KeyValuePair<int, int>(row, col));
+                    coordinatesHS.Add(new KeyValuePair<int, int>(row, col + 2));
+                    coordinatesHS.Add(new KeyValuePair<int, int>(row + 1, col + 1));
+                    coordinatesHS.Add(new KeyValuePair<int, int>(row + 2, col));
+                    coordinatesHS.Add(new KeyValuePair<int, int>(row + 2, col + 2));
+                }
+            }
+        }
+
+        //Print result
+        KeyValuePair<int, int> currentPair = new KeyValuePair<int, int>();
+        for (int row = 0; row < matrix.Count; row++)
+        {
+            for (int col = 0; col < matrix[row].Length; col++)
+            {
+                currentPair = new KeyValuePair<int, int>(row, col);
+                if (!coordinatesHS.Contains(currentPair))
+                {
+                    Console.Write(matrix[row][col]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static bool IsPlus(List<char[]> matrix, int row, int col)
+    {
+        if (matrix[row + 1].Length >= col + 2 && matrix[row + 2].Length >= col + 3 &&
+            matrix[row][col].ToString().ToUpper() == matrix[row][col + 2].ToString().ToUpper() &&
+            matrix[row][col].ToString().ToUpper() == matrix[row + 1][col + 1].ToString().ToUpper() &&
+            matrix[row][col].ToString().ToUpper() == matrix[row + 2][col].ToString().ToUpper() &&
+            matrix[row][col].ToString().ToUpper() == matrix[row + 2][col + 2].ToString().ToUpper())
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Advanced C#/3. MultiDimArr-Sets-Dictionaries/O. VladkoNotebook/VladkoNotebook.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class VladkoNotebook
+{
+    static void Main()
+    {
+        var notebook = new SortedDictionary<string, SortedDictionary<string, dynamic>>();
+        string[] data = new string[3];
+        string page = Console.ReadLine();
+
+        //Fill the notebook
+        while (page != "END")
+        {
+            data = page.Split('|');
+            string color = data[0];
+            string category = data[1];
+            string statistic = data[2];
+            if (!notebook.ContainsKey(color))
+            {
+                notebook[color] = new SortedDictionary<string, dynamic>();
+            }
+            if (category == "name")
+            {
+                if (!notebook[color].ContainsKey(category))
+                {
+                    notebook[color][category] = statistic;
+                }
+            }
+            else if (category == "age")
+            {
+                if (!notebook[color].ContainsKey(category))
+                {
+                    notebook[color][category] = statistic;
+                }
+            }
+            else
+            {
+                if (!notebook[color].ContainsKey("opponents"))
+                {
+                    notebook[color]["opponents"] = new List<string>();
+                    notebook[color]["rank"] = 1.0M;
+                    notebook[color]["wins"] = 1.0M;
+                    notebook[color]["losses"] = 1.0M;
+
+                }
+                notebook[color]["opponents"].Add(statistic);
+                if (category == "win")
+                {
+                    notebook[color]["wins"] += 1.0M;
+                }
+                else
+                {
+                    notebook[color]["losses"] += 1.0M;
+                }
+            }
+            page = Console.ReadLine();
+        }
+
+        //Remove incomplete entries
+        var keysToRemove = new List<string>();
+        foreach (var pair in notebook)
+        {
+            if (!notebook[pair.Key].ContainsKey("age") || !notebook[pair.Key].ContainsKey("name"))
+            {
+                keysToRemove.Add(pair.Key);
+            }
+        }
+        foreach (var key in keysToRemove)
+        {
+            notebook.Remove(key);
+        }
+
+        //Check for empty opponents
+        foreach (var pair in notebook)
+        {
+            if (!notebook[pair.Key].ContainsKey("opponents"))
+            {
+                notebook[pair.Key]["opponents"] = new List<string>();
+                notebook[pair.Key]["opponents"].Add("(empty)");
+                notebook[pair.Key]["rank"] = 1.0M;
+                notebook[pair.Key]["wins"] = 1.0M;
+                notebook[pair.Key]["losses"] = 1.0M;
+            }
+        }
+
+        //Sort Opponents and get ranks
+        foreach (var pair in notebook)
+        {
+            notebook[pair.Key]["opponents"].Sort();
+            decimal rank = notebook[pair.Key]["wins"] / notebook[pair.Key]["losses"];
+            notebook[pair.Key]["rank"] = rank.ToString("F2");
+            notebook[pair.Key].Remove("wins");
+            notebook[pair.Key].Remove("losses");
+        }
+
+        //Print result
+        if (notebook.Count == 0)
+        {
+            Console.WriteLine("No data recovered.");
+        }
+        else
+        {
+            foreach (var pair in notebook)
+            {
+                Console.WriteLine("Color: {0}", pair.Key);
+                foreach (var subpair in pair.Value)
+                {
+                    Console.WriteLine("-{0}: {1}", subpair.Key, string.Join(", ", subpair.Value));
+                }
+            }
+        }     
+    }
+}
