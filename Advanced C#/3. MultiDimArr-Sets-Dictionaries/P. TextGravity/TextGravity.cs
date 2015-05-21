434150e3eacc7ddb6c0c34edc744efbacea2e0e2
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-21 14:02:15 +0300

    Added TextGravity.cs and ClearingCommands.cs

---
+++ Advanced C#/3. MultiDimArr-Sets-Dictionaries/P. TextGravity/TextGravity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TextGravity
+{
+    static void Main()
+    {
+        int matrixCols = int.Parse(Console.ReadLine());
+        char[] text = Console.ReadLine().ToCharArray();
+
+        //Fill the matrix
+        int matrixRows = 0;
+        int matrixSize = 0;
+        if (matrixCols == 1)
+        {
+            matrixRows = text.Length;
+        }
+        else if (text.Length % matrixCols == 0)
+        {
+            matrixRows = text.Length/matrixCols;
+        }
+        else
+        {
+            matrixSize = text.Length + (matrixCols - text.Length % matrixCols);
+            matrixRows = matrixSize / matrixCols;
+        }        
+        char[,] matrix = new char[matrixRows,matrixCols];
+        int counter = 0;
+        for (int i = 0; i < matrixRows; i++)
+        {
+            for (int j = 0; j < matrixCols; j++)
+            {
+                if (counter < text.Length)
+                {
+                    matrix[i,j] = text[counter];
+                    counter++;
+                }
+                else
+                {
+                    matrix[i,j] = ' ';
+                }
+            }
+        }
+
+        //Colapse the matrix
+        for (int row = matrixRows - 1; row > -1; row--)
+        {
+            for (int col = 0; col < matrixCols; col++)
+            {
+                if (matrix[row,col] == ' ')
+                {
+                    int nextRow = row - 1;
+                    while (nextRow > -1)
+                    {
+                        if (matrix[nextRow, col] != ' ')
+                        {
+                            char temp = matrix[row, col];
+                            matrix[row, col] = matrix[nextRow, col];
+                            matrix[nextRow, col] = temp;
+                            break;
+                        }
+                        nextRow--;
+                    }
+                }
+            }
+        }
+
+        //Print the matrix
+        Console.Write("<table>");
+        for (int i = 0; i < matrixRows; i++)
+        {
+            Console.Write("<tr>");
+            for (int j = 0; j < matrixCols; j++)
+            {
+                Console.Write("<td>{0}</td>", matrix[i, j]);
+            }
+            Console.Write("</tr>");
+        }
+        Console.Write("</table>");
+    }
+}
---
+++ Advanced C#/3. MultiDimArr-Sets-Dictionaries/Q. ClearingCommands/ClearingCommands.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ClearingCommands
+{
+    static void Main()
+    {
+        //Fill the matrix
+        List<char[]> matrix = new List<char[]>();
+        string inputLine = Console.ReadLine();
+        while (inputLine != "END")
+        {
+            matrix.Add(inputLine.ToCharArray());
+            inputLine = Console.ReadLine();
+        }
+
+        //Clean the matrix
+        for (int row = 0; row < matrix.Count; row++)
+        {
+            for (int col = 0; col < matrix[row].Length; col++)
+            {   
+                if (matrix[row][col] == '>')
+                {
+                    int nextRow = row;
+                    int nextCol = col + 1;
+                    while (nextCol < matrix[row].Length && matrix[nextRow][nextCol] != '>' && matrix[nextRow][nextCol] != '<' &&
+                        matrix[nextRow][nextCol] != 'v' && matrix[nextRow][nextCol] != '^')
+                    {
+                        matrix[nextRow][nextCol] = ' ';
+                        nextCol++;
+                    }
+                    col = nextCol - 1;
+                }
+                else if (matrix[row][col] == '<')
+                {
+                    int nextRow = row;
+                    int nextCol = col -1;
+                    while (nextCol > -1 && matrix[nextRow][nextCol] != '>' && matrix[nextRow][nextCol] != '<' &&
+                        matrix[nextRow][nextCol] != 'v' && matrix[nextRow][nextCol] != '^')
+                    {
+                        matrix[nextRow][nextCol] = ' ';
+                        nextCol--;
+                    }
+                }
+                else if (matrix[row][col] == 'v')
+                {
+                    int nextRow = row + 1;
+                    int nextCol = col;
+                    while (nextRow < matrix.Count && matrix[nextRow][nextCol] != '>' && matrix[nextRow][nextCol] != '<' &&
+                        matrix[nextRow][nextCol] != 'v' && matrix[nextRow][nextCol] != '^')
+                    {
+                        matrix[nextRow][nextCol] = ' ';
+                        nextRow++;
+                    }
+                }
+                else if (matrix[row][col] == '^')
+                {
+                    int nextRow = row - 1;
+                    int nextCol = col;
+                    while (nextRow > -1 && matrix[nextRow][nextCol] != '>' && matrix[nextRow][nextCol] != '<' &&
+                        matrix[nextRow][nextCol] != 'v' && matrix[nextRow][nextCol] != '^')
+                    {
+                        matrix[nextRow][nextCol] = ' ';
+                        nextRow--;
+                    }
+                }
+            }
+        }
+
+        //Print the matrix
+        for (int row = 0; row < matrix.Count; row++)
+        {
+            Console.Write("<p>");
+            for (int col = 0; col < matrix[row].Length; col++)
+            {
+                if (matrix[row][col] == '>')
+                {
+                    Console.Write("&gt;");
+                }
+                else if (matrix[row][col] == '<')
+                {
+                    Console.Write("&lt;");
+                }
+                else
+                {
+                    Console.Write(matrix[row][col]);
+                }   
+            }
+            Console.Write("</p>");
+            Console.WriteLine();
+        }
+    }
+}
