f6ddadbe53eaec17f00d81068a06983af47f47b1
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-22 01:18:47 +0300

    Added QueryMess.cs

---
+++ Advanced C#/5. Regular-Expressions/G. QueryMess/QueryMess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class QueryMess
+{
+    static void Main()
+    {
+        string text = Console.ReadLine().Replace("%20", " ").Replace("+", " ");
+        string questionsPattern = @".+(?=\?)\?";
+        string emptySpacesPattern = @"\s{2,}";
+        string keyValuePattern = @"(?:[\&\s]*)(?<key>[^[\=\s]+(?:[\s][^\=\s]+)*)(?:[\=\s]*)(?<value>[^\&\s]+(?:[\s][^\&\s]+)*)(?:[\s\&]*)";        
+        Regex regex = new Regex(keyValuePattern);
+        MatchCollection matches;
+        var pairs = new Dictionary<string, List<string>>();
+        while (text != "END")
+        {
+            text = Regex.Replace(text, questionsPattern, " ");
+            text = Regex.Replace(text, emptySpacesPattern, " ");
+            matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                string key = match.Groups["key"].ToString();
+                string value = match.Groups["value"].ToString();
+                if (!pairs.ContainsKey(key))
+                {
+                    pairs.Add(key, new List<string>());
+                }
+                pairs[key].Add(value);
+            }
+
+            foreach (var pair in pairs)
+            {
+                Console.Write("{0}=[{1}]", pair.Key, string.Join(", ", pair.Value));
+            }
+            Console.WriteLine();
+
+            pairs.Clear();
+            text = Console.ReadLine().Replace("%20", " ").Replace("+", " ").Trim();
+        }
+    }
+}
