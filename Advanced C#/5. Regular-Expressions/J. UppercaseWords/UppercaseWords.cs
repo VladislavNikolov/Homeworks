825f4e259b83fbf6235a3e07c15ae39f9ccf1bd9
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-21 15:48:09 +0300

    Added UppercaseWords.cs

---
+++ Advanced C#/5. Regular-Expressions/J. UppercaseWords/UppercaseWords.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class UppercaseWords
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        Regex regex = new Regex(@"[a-zA-Z]+");
+        while (text != "END")
+        {
+            MatchCollection matches = regex.Matches(text);
+            string result = text;
+            foreach (var match in matches)
+            {
+                string normal = match.ToString();
+                if (IsAllUpper(normal))
+                {
+                    string reversed = ReverseString(normal);
+                    if (normal == reversed)
+                    {
+                        string doubled = DoubleEachCharacter(normal);
+                        string pattern = @"(?<![a-zA-Z])(" + normal + @")(?![a-zA-Z])";
+                        result = Regex.Replace(result, pattern, doubled);
+                    }
+                    else
+                    {
+                        result = result.Replace(normal, reversed);
+                    }
+                }
+            }
+            result = SecurityElement.Escape(result);
+            Console.WriteLine(result);
+            text = Console.ReadLine();
+        }
+    }
+
+    static bool IsAllUpper(string match)
+    {
+        for (int i = 0; i < match.Length; i++)
+        {
+            if (!Char.IsUpper(match[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    static string ReverseString(string match)
+    {
+        char[] arr = match.ToCharArray();
+        Array.Reverse(arr);
+        string reversed = new string(arr);
+
+        return reversed;
+    }
+
+    static string DoubleEachCharacter(string match)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < match.Length; i++)
+        {
+            sb.Append(match[i]);
+            sb.Append(match[i]);
+        }
+        return sb.ToString();
+    }
+}
