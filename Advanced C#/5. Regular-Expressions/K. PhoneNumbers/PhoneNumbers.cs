5c8dd23656aeed42904d6ec06fbef8ac299ea573
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-23 21:56:53 +0300

    Added PhoneNumbers.cs and TheNumbers.cs

---
+++ Advanced C#/5. Regular-Expressions/K. PhoneNumbers/PhoneNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class PhoneNumbers
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        string pattern = @"(?<name>[A-Z][a-zA-Z]*)(?!(?:[^\+\d]*[A-Z]))(?:[^\+\d]*)(?<phone>[\+\d]*(?:[\(\)\/\.\-\s]*\d)*)";
+        var phonebook = new Dictionary<string, string>();
+        while (text != "END")
+        {
+            MatchCollection matches = Regex.Matches(text, pattern);
+            foreach (Match match in matches)
+            {
+                string name = match.Groups["name"].ToString();
+                string phone = match.Groups["phone"].ToString();
+                StringBuilder sb = new StringBuilder();
+                foreach (char ch in phone)
+                {
+                    if (char.IsNumber(ch) || ch == '+')
+                    {
+                        sb.Append(ch);
+                    }
+                }
+                if (sb.Length > 1)
+                {
+                    phonebook.Add(name, sb.ToString());
+                }
+            }
+            text = Console.ReadLine();
+        }
+
+        if (phonebook.Count == 0)
+        {
+            Console.WriteLine("<p>No matches!</p>");
+        }
+        else
+        {
+            Console.Write("<ol>");
+            foreach (var pair in phonebook)
+            {
+                Console.Write("<li><b>{0}:</b> {1}</li>", pair.Key, pair.Value);
+            }
+            Console.Write("</ol>");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Advanced C#/5. Regular-Expressions/L. TheNumbers/TheNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class TheNumbers
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        List<string> numbers = new List<string>();
+        MatchCollection matches = Regex.Matches(text, @"\d+");
+        foreach (Match match in matches)
+        {
+            int dec = int.Parse(match.ToString());
+            string hex = "0x" + dec.ToString("X").PadLeft(4, '0');
+            numbers.Add(hex);
+        }
+        Console.WriteLine(string.Join("-", numbers));
+    }
+}
