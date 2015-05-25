de7c6c302e071645ff74f081a328dcd5cb6addf5
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-25 22:45:42 +0300

    Added StudentsByGroup.cs

---
+++ Advanced C#/7. Functional-Programming/B. StudentsByGroup/StudentsByGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentsByGroup
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student("Ivan", "Vladev", 18, "1545", "021234", "ivan@gmail.com", new List<int> {5, 3, 5, 6}, 1),
+            new Student("Pesho", "Petkov", 26, "1535", "3592321638", "peshev@abv.bg", new List<int> {5, 2, 5, 6}, 1),
+            new Student("Petko", "Goskov", 19, "1546", "07113", "petkov@abv.bg", new List<int> {5, 2, 5, 6}, 2),
+            new Student("Goshko", "Peshev", 19, "1542", "083545", "goshov@gmail.com", new List<int> {5, 2, 5, 5}, 2),
+            new Student("Mariika", "Roseva", 25, "1555", "+359 2", "niama@ght.gh", new List<int> {6, 2, 6, 4}, 1),
+            new Student("Rositza", "Marieva", 20,"1556", "112345", "pakmail@abv.bg", new List<int> {4, 2, 6, 3}, 2),
+            new Student("Vlado", "Petkov", 36, "1545", "524354", "zashto@hotmail.com", new List<int> {6, 2, 6, 6}, 1)
+        };
+
+        var studentQuery =
+            from student in students
+            where student.GroupNumber == 2
+            orderby student.FirstName
+            select student;
+
+        Console.WriteLine("Students from group 2:");
+        foreach (var student in studentQuery)
+        { 
+            Console.WriteLine(student.FirstName);
+        }
+    }
+}
