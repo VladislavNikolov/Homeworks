9b4048af94865e39e598f7d727e90a929200824e
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-05-30 18:58:05 +0300

    Added ExtractHyperlinks.cs and SemanticHTML.cs

---
+++ Advanced C#/5. Regular-Expressions/F. ExtractHyperlinks/ExtractHyperlinks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExtractHyperlinks
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        string line = Console.ReadLine();
+
+        while (line != "END")
+        {
+            sb.Append(line);
+            line = Console.ReadLine();
+        }
+
+        string text = sb.ToString();
+
+        Regex rgx = new Regex(@"<a\s+(?:[^>]+\s+)?href\s*=\s*(?:'([^']*)'|""([^""]*)""|([^\s>]+))[^>]*>");
+        Match match = rgx.Match(text);
+
+        while (match != Match.Empty)
+        {
+            for (int i = 1; i < match.Groups.Count; i++)
+            {
+                if (match.Groups[i].Value != String.Empty)
+                {
+                    Console.WriteLine(match.Groups[i].Value);
+                }
+            }
+
+            match = match.NextMatch();
+        }
+    }
+}
