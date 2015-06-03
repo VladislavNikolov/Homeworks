c6b82ea16bf3be2cc458cf2a2639e9c7680368a3
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-06-03 14:00:43 +0300

    Added Persons.cs and LaptopShop.cs

---
+++ Object-Oriented Programming/1. Defining-Classes/A. Persons/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Person
+{
+    private string name;
+    private int age;
+    private string email;
+
+    public Person(string name, int age, string email)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+
+    public Person(string name, int age) : this(name, age, null)
+    { 
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name should not be empty");
+            }
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set
+        {
+            if (value < 1 || value > 100)
+            {
+                throw new ArgumentOutOfRangeException("Invalid age");
+            }
+            this.age = value;
+        }
+    }
+
+    public string Email
+    {
+        get { return this.email; }
+        set
+        {
+            if (value == null || value.Contains("@"))
+            {
+                this.email = value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid email");
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Name: {0}, Age: {1}, Email: {2}", 
+            this.Name, this.Age, this.Email);
+    }
+}
