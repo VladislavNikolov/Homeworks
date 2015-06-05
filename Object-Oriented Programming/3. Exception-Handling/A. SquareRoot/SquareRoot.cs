c5bd5909d1246a761526b1676e72424fcd9a9b10
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-06-05 16:15:26 +0300

    Added SquareRoot.cs and EnterNumbers.cs

---
+++ Object-Oriented Programming/3. Exception-Handling/A. SquareRoot/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class SquareRoot
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a positive integer:");
+        string readLine = Console.ReadLine();
+        try
+        {
+            int number = int.Parse(readLine);
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Number cannot be negative");
+            }
+
+            Console.WriteLine("The square root of {0} is {1}", number, Math.Sqrt(number));
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ Object-Oriented Programming/3. Exception-Handling/B. EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EnterNumbers
+{
+    public static void Main()
+    {
+        int start = 1;
+        int end = 100;
+        List<int> allNums = new List<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            start = ReadNumber(start, end);
+            if (start == 0)
+            {                
+                break;
+            }
+
+            allNums.Add(start);
+        }
+
+        if (allNums.Count < 10)
+        {
+            Console.WriteLine("You done goofed up, foresight 0/20, you didn't get to 10 numbers.");
+        }
+        else
+        {
+            Console.WriteLine("You win! Your numbers are:{0}{1}", Environment.NewLine, string.Join(", ", allNums));
+        }
+    }
+
+    private static int ReadNumber(int start, int end)
+    {
+        while (true)
+        {
+            try
+            {   
+                // If start and end are subsequent numbers you cannot enter a valid integer             
+                if (start == end - 1)
+                {
+                    return 0;
+                }
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (number <= start || number >= end)
+                {
+                    throw new ArgumentOutOfRangeException("Number should be between start and end");
+                }
+
+                return number;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number");
+            }
+        }        
+    }
+}
