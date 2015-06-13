e0eed010a1ca43d7683db2a6afc0c8bffd0a7452
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-06-13 18:36:20 +0300

    Added D. GenericListVersion

---
+++ Object-Oriented Programming/6. Other-Types-In-OOP/D. GenericListVersion/GenericListVersion.cs
@@ -0,0 +1,18 @@
+﻿namespace D.GenericListVersion
+{
+    using System;
+    using C.GenericList;
+
+    class GenericListVersion
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(GenericList<>);
+            object[] versionAttributes = type.GetCustomAttributes(typeof(VersionAttribute), true);
+            foreach (VersionAttribute attribute in versionAttributes)
+            {
+                Console.WriteLine("Version: " + attribute.Major + "." + attribute.Minor);
+            }
+        }
+    }
+}
---
+++ Object-Oriented Programming/6. Other-Types-In-OOP/D. GenericListVersion/VersionAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace D.GenericListVersion
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface |
+        AttributeTargets.Enum | AttributeTargets.Method, AllowMultiple = false)]
+    class VersionAttribute : Attribute
+    {
+        public VersionAttribute(byte major, byte minor)
+        {
+            this.Major = major;
+            this.Minor = minor;
+        }
+
+        public byte Major { get; private set; }
+
+        public byte Minor { get; private set; }
+    }
+}
