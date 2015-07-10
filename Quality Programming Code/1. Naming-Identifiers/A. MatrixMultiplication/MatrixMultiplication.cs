240b1c37d88424ac8f267e70004d93537b13eb7b
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-07-10 13:15:51 +0300

    Added Quality Programming Code

---
+++ Quality Programming Code/1. Naming-Identifiers/A. MatrixMultiplication/MatrixMultiplication.cs
@@ -0,0 +1,47 @@
+﻿namespace MatrixMultiplication
+{
+    using System;
+
+    public class MatrixMultiplication
+    {
+        public static void Main(string[] args)
+        {
+            var firstMatrix = new double[,] { { 1, 3 }, { 5, 7 } };
+            var secondMatrix = new double[,] { { 4, 2 }, { 1, 5 } };
+            var product = MultiplyMatrices(firstMatrix, secondMatrix);
+
+            for (int row = 0; row < product.GetLength(0); row++)
+            {
+                for (int col = 0; col < product.GetLength(1); col++)
+                {
+                    Console.Write(product[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static double[,] MultiplyMatrices(double[,] firstMatrix, double[,] secondMatrix)
+        {
+            if (firstMatrix.GetLength(1) != secondMatrix.GetLength(0))
+            {
+                throw new ArgumentException("The columns of the first matrix should be equal to the rows of the second matrix");
+            }
+
+            var lengthOfFirstMatrix = firstMatrix.GetLength(1);
+            var product = new double[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+            for (int i = 0; i < product.GetLength(0); i++)
+            {
+                for (int j = 0; j < product.GetLength(1); j++)
+                {
+                    for (int k = 0; k < lengthOfFirstMatrix; k++)
+                    {
+                        product[i, j] += firstMatrix[i, k] * secondMatrix[k, j];
+                    }                        
+                }                    
+            }
+                
+            return product;
+        }
+    }
+}
---
+++ Quality Programming Code/1. Naming-Identifiers/C. MinesweeperGame/Ranking.cs
@@ -0,0 +1,15 @@
+﻿namespace MinesweeperGame
+{
+    public class Ranking
+    {
+        public Ranking(string name, int points)
+        {
+            this.Name = name;
+            this.Points = points;
+        }
+
+        public string Name { get; private set; }
+
+        public int Points { get; private set; }       
+    }
+}
