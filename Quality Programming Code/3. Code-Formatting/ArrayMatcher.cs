98a2323288b15bbc425c4d31111e2dc29414119f
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2015-07-12 14:34:17 +0300

    Sync

---
+++ Quality Programming Code/3. Code-Formatting/ArrayMatcher.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split('\\');
+        char[] array1 = input[0].ToCharArray();
+        char[] array2 = input[1].ToCharArray();
+        string cmd = input[2];
+        List<char> array3 = new List<char>();
+
+        if (cmd == "join")
+        {
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+
+                for (int j = 0; j < array2.Length; j++)
+                {
+
+                    if (array1[i] == array2[j])
+                    {
+                        array3.Add(array1[i]);
+                    }
+                }
+            }
+        }
+        else if (cmd == "right exclude")
+        {
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+
+                int counter = 0;
+
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0)
+                {
+                    array3.Add(array1[i]);
+                }
+            }
+        }
+        else if (cmd == "left exclude")
+        {
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+
+                int counter = 0;
+
+                for (int j = 0; j < array1.Length; j++)
+                {
+
+                    if (array2[i] == array1[j])
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0)
+                {
+                    array3.Add(array2[i]);
+                }
+            }
+        }
+        array3.Sort();
+        foreach (char item in array3)
+        {
+            Console.Write(item);
+        }
+        Console.WriteLine();
+
+    }
+}
---
+++ Quality Programming Code/3. Code-Formatting/ArrayMatcher_reformatted.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class ArrayMatcher
+{
+    static void Main()
+    {
+        string[] userInput = Console.ReadLine().Split('\\');
+        char[] leftArray = userInput[0].ToCharArray();
+        char[] rightArray = userInput[1].ToCharArray();
+        string command = userInput[2];
+        List<char> outputArray = new List<char>();
+
+        if (command == "join")
+        {
+            for (int i = 0; i < leftArray.Length; i++)
+            {
+                for (int j = 0; j < rightArray.Length; j++)
+                {
+                    if (leftArray[i] == rightArray[j])
+                    {
+                        outputArray.Add(leftArray[i]);
+                    }
+                }
+            }
+        }
+        else if (command == "right exclude")
+        {
+            for (int i = 0; i < leftArray.Length; i++)
+            {
+                bool areArraysMatched = false;
+
+                for (int j = 0; j < rightArray.Length; j++)
+                {
+                    if (leftArray[i] == rightArray[j])
+                    {
+                        areArraysMatched = true;
+                        break;
+                    }
+                }
+
+                if (!areArraysMatched)
+                {
+                    outputArray.Add(leftArray[i]);
+                }
+            }
+        }
+        else if (command == "left exclude")
+        {
+            for (int i = 0; i < rightArray.Length; i++)
+            {
+                bool areArraysMatched = false;
+
+                for (int j = 0; j < leftArray.Length; j++)
+                {
+                    if (rightArray[i] == leftArray[j])
+                    {
+                        areArraysMatched = true;
+                        break;
+                    }
+                }
+
+                if (!areArraysMatched)
+                {
+                    outputArray.Add(rightArray[i]);
+                }
+            }
+        }
+
+        outputArray.Sort();
+
+        Console.WriteLine(string.Join(string.Empty, outputArray));
+    }
+}
